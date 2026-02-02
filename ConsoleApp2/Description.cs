d92c1f0dc077632ae9346fd1b77f0f5f54a3876f
Ujvari.Csongor <Ujvari.Csongor@C112-012>
2026-02-02 14:18:26 +0100

    Add project files.

---
+++ ConsoleApp2/Description.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Description
+    {
+        public int id { get; set; }
+        public int userId { get; set; }
+        public int height { get; set; }
+        public int weight { get; set; }
+        public string gender { get; set; }
+        public string interestedIn { get; set; }
+    }
+}
---
+++ ConsoleApp2/ServerConnection.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2;
+
+internal class ServerConnection
+{
+
+    HttpClient client = new HttpClient();
+    string Token = String.Empty;
+    public ServerConnection(string baseUrl)
+    {
+        client.BaseAddress = new Uri(baseUrl);
+
+    }
+    public async Task<bool> Register(string username, string password, string email, DateOnly birthDate)
+    {
+        try
+        {
+            var jsonData = new
+            {
+                username,
+                password,
+                email,
+                birthDate
+            };
+            string jsonString = JsonSerializer.Serialize(jsonData);
+            StringContent sendTs = new StringContent(jsonString, Encoding.UTF8);
+            HttpResponseMessage response = await client.PostAsync("/register", sendTs);
+            
+            Message oneMsg = JsonSerializer.Deserialize<Message>(await response.Content.ReadAsStringAsync());
+            response.EnsureSuccessStatusCode();
+            
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return false;
+        }
+        return true;
+    }
+    public async Task<bool> Login(string name, string password)
+    {
+        try
+        {
+            var jsonData = new
+            {
+                name,
+                password
+            };
+            string jsonString = JsonSerializer.Serialize(jsonData);
+            StringContent sendTs = new StringContent(jsonString, Encoding.UTF8);
+            HttpResponseMessage response = await client.PostAsync("/Login", sendTs);
+
+            Message oneMsg = JsonSerializer.Deserialize<Message>(await response.Content.ReadAsStringAsync());
+            response.EnsureSuccessStatusCode();
+
+            Token = oneMsg.token;
+            client.DefaultRequestHeaders.Add("authorization","Bearer " + Token);
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return false;
+        }
+        return true;
+    }
+    public async Task<bool> ChangePassword(string newPassword)
+    {
+        try
+        {
+            var jsonData = new
+            {
+                newPassword
+            };
+            string jsonString = JsonSerializer.Serialize(jsonData);
+            StringContent sendTs = new StringContent(jsonString, Encoding.UTF8);
+            HttpResponseMessage response = await client.PatchAsync("/profile", sendTs);
+
+            Message oneMsg = JsonSerializer.Deserialize<Message>(await response.Content.ReadAsStringAsync());
+            response.EnsureSuccessStatusCode();
+
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return false;
+        }
+        return true;
+    }
+    public async Task<bool> ChangeUsername(string newUsername)
+    {
+        try
+        {
+            var jsonData = new
+            {
+                newUsername
+            };
+            string jsonString = JsonSerializer.Serialize(jsonData);
+            StringContent sendTs = new StringContent(jsonString, Encoding.UTF8);
+            HttpResponseMessage response = await client.PutAsync("/profile", sendTs);
+
+            Message oneMsg = JsonSerializer.Deserialize<Message>(await response.Content.ReadAsStringAsync());
+            response.EnsureSuccessStatusCode();
+
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return false;
+        }
+        return true;
+    }
+    public async Task DeleteUser()
+    {
+        try
+        {
+            HttpResponseMessage response = await client.DeleteAsync("/profile");
+
+            Message oneMsg = JsonSerializer.Deserialize<Message>(await response.Content.ReadAsStringAsync());
+            response.EnsureSuccessStatusCode();
+
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
